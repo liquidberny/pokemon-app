eb763e28774a2d8fd892a46aa51fd07389c3162f
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-09-16 12:39:04 -0700

    estructura, enumeraciones, tipado, propiedades computadas, interfaces

--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -1,21 +1,23 @@
 <template>
-    <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+    <section v-if="isLoading" class="flex flex-col justify-center items-center w-screen h-screen">
         <h1 class="text-3xl">Espere por favor</h1>
         <h2 class="animate-pulse">Cargando Pokémons...</h2>
     </section>
-    <section class="flex flex-col justify-center items-center w-screen h-screen">
-        <h1>Quien es este Pokémon?</h1>
+    <section v-else="" class="flex flex-col justify-center items-center w-screen h-screen">
+        <h1 class="m-5">Quien es este Pokémon?</h1>
 
         <!-- Pokemon Picture -->
         <PokemonPicture/>
         <!-- Pokemon Options -->
-
+        <PokemonOptions/>
     </section>
 </template>
 
 <script setup lang="ts">
+import PokemonOptions from '../components/PokemonOptions.vue';
 import PokemonPicture from '../components/PokemonPicture.vue';
+import { usePokemonGame } from '../composables/usePokemonGame';
 
-
+const {gameStatus ,isLoading}= usePokemonGame();
 </script>
 
