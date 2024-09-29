d93b13e5b82703248bd4f4877de3e1daa7e01249
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-09-28 20:39:57 -0700

    pruebas composables

--- tests/modules/pokemon/composables/usePokemonGame.test.ts
+++ tests/modules/pokemon/composables/usePokemonGame.test.ts
@@ -1,11 +1,10 @@
 import { usePokemonGame } from '@/modules/pokemon/composables/usePokemonGame';
-import MockAdapter from 'axios-mock-adapter'
+import MockAdapter from 'axios-mock-adapter';
 import { withSetup } from '../../../utils/with-setup';
 import { GameStatus } from '@/modules/pokemon/interfaces';
 import { flushPromises } from '@vue/test-utils';
 import { pokemonApi } from '@/modules/pokemon/api/pokemonApi';
 import { pokemonListFake } from '../data/fake-pokemon';
-
 
 const mockPokemonApi = new MockAdapter(pokemonApi);
 
@@ -15,21 +14,57 @@ mockPokemonApi.onGet('/?limit=151').reply(200, {
 
 describe('usePokemonGame', () => {
   test('should initialize with the correct default values', async () => {
-    const [results, app ] = withSetup(usePokemonGame);
+    const [results, app] = withSetup(usePokemonGame);
 
     expect(results.gameStatus.value).toBe(GameStatus.Playing);
     expect(results.isLoading.value).toBe(true);
     expect(results.pokemonOptions.value).toEqual([]);
     expect(results.randomPokemon.value).toBe(undefined);
-    
+
     await flushPromises();
-    
+
     expect(results.isLoading.value).toBe(false);
     expect(results.pokemonOptions.value.length).toBe(4);
     expect(results.randomPokemon.value).toEqual({
       id: expect.any(Number),
-      name: expect.any(String)
+      name: expect.any(String),
     });
+  });
+  test('should correcly handle getNextRound', async () => {
+    const [results] = withSetup(usePokemonGame);
+    await flushPromises();
+
+    results.gameStatus.value = GameStatus.Won;
+  });
+
+  test('should correctly handle getNextRound', async () => {
+    const [results] = withSetup(usePokemonGame);
+    await flushPromises();
+
+    results.gameStatus.value = GameStatus.Won;
+
+    //estimulo
+    results.getNextRound(5);
+    expect(results.gameStatus.value).toBe(GameStatus.Playing);
+    expect(results.pokemonOptions.value).toHaveLength(5);
+  });
+
+  test('should correctly handle getNextRound and return different pokemons', async () => {
+    const [results] = withSetup(usePokemonGame);
+    await flushPromises();
+
+
+    //estimulo
+    const first = [...results.pokemonOptions.value].map(p  => p.name)
+
+    results.getNextRound(4);
+    const second = [...results.pokemonOptions.value]
+
+
+    second.forEach((pokemon) => {
+      expect(first).not.toContain(pokemon.name)
+    })
+
 
   });
 });
