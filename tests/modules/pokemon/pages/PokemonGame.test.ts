d174afc55b1ce0452d4ee89792ce1042df41fe5f
liquidberny <bernardo.urquijo.bu@gmail.com>
2024-09-29 14:12:48 -0700

    pruebas en PokemonGame-Componentes

--- tests/modules/pokemon/pages/PokemonGame.test.ts
+++ tests/modules/pokemon/pages/PokemonGame.test.ts
@@ -8,6 +8,25 @@ vi.mock('@/modules/pokemon/composables/usePokemonGame', () => ({
   usePokemonGame: vi.fn(),
 }));
 
+const pokemons = [
+  {
+    name: 'bulbasaur',
+    id: 1,
+  },
+  {
+    name: 'ivysaur',
+    id: 2,
+  },
+  {
+    name: 'venusaur',
+    id: 3,
+  },
+  {
+    name: 'charmander',
+    id: 4,
+  },
+];
+
 describe('<PokemonGame/>', () => {
   test('should initializae with default values', () => {
     (usePokemonGame as Mock).mockReturnValue({
@@ -22,7 +41,24 @@ describe('<PokemonGame/>', () => {
     const wrapper = mount(PokemonGame);
     expect(wrapper.get('h1').text()).toBe('Espere por favor');
     expect(wrapper.get('h1').classes()).toEqual(['text-3xl']);
-    expect(wrapper.get('h3').text()).toBe('Cargando Pokémons...');
-    expect(wrapper.get('h3').classes()).toEqual(['animate-pulse']);
+
+    expect(wrapper.get('h2').text()).toBe('Cargando Pokémons...');
+    expect(wrapper.get('h2').classes()).toEqual(['animate-pulse']);
+  });
+
+  test('should render <PokemonPicture/> and <PokemonOptions />', () => {
+    (usePokemonGame as Mock).mockReturnValue({
+      gameStatus: GameStatus.Playing,
+      isLoading: false,
+      randomPokemon: pokemons.at(0),
+      pokemonOptions: pokemons,
+      checkAnswer: vi.fn(),
+      getNextRound: vi.fn(),
+    });
+
+    const wrapper = mount(PokemonGame);
+    const imageURL =
+      'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg';
+    expect(wrapper.find('img').attributes('src')).toBe(imageURL);
   });
 });
